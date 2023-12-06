accb35574b62a6ac87a66f7a908fc214ad5f8073
muzamil khan <muzamilfootballer111111@gmail.com>
2023-12-05 17:57:43 -0800

    first msg

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,17 @@
+import SignInBtns from "@/components/SignInBtns";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+
+export default async function SignIn() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <SignInBtns />
+  )
+}
